b51bc39d566119b71ec6cc16ac395e9a6760f153
Solswinters <solswinters@users.noreply.github.com>
2025-11-17 20:45:48 +0100

    feat: add throttle helper utility

---
+++ src/lib/helpers/throttle.ts
@@ -0,0 +1,20 @@
+/**
+ * Throttle function utility
+ */
+
+export function throttle<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean = false
+
+  return function executedFunction(...args: Parameters<T>) {
+    if (!inThrottle) {
+      func(...args)
+      inThrottle = true
+      setTimeout(() => {
+        inThrottle = false
+      }, limit)
+    }
+  }
+}
