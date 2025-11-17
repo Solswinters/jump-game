276ca07a9dd173240e88b29d742cac7a112c430f
Solswinters <solswinters@users.noreply.github.com>
2025-11-17 20:42:15 +0100

    feat: add constants index file

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Central constants exports
+ */
+
+export * from './errors'
+export * from './routes'
+export * from './server'
+export * from './socket'
+export * from './socket-events'
+export * from './theme'
+export * from './validation'
