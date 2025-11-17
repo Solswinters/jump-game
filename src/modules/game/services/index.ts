4574f8b84b15bb733e9bb8c2293fcc779762b24e
Solswinters <solswinters@users.noreply.github.com>
2025-11-17 20:47:31 +0100

    feat: add game services index

---
+++ src/modules/game/services/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Game services exports
+ */
+
+export * from './ApiClient'
+export * from './game-session-manager'
+export * from './GameScoreService'
+export * from './GameStateService'
+export * from './high-score-tracker'
+export * from './leaderboard-service'
+export * from './player-stats'
+export * from './session-manager'
