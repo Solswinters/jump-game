c3d30763cad95971005d31fcf6f0bbef6dd8ca30
Solswinters <solswinters@users.noreply.github.com>
2025-11-19 14:17:39 +0100

    feat: add multiplayer module barrel exports
    - Create comprehensive module exports
    - Export all components, hooks, and services
    - Include types, utils, and constants
    - Add provider export
    - Organize exports by category

--- src/modules/multiplayer/index.ts
+++ src/modules/multiplayer/index.ts
@@ -2,7 +2,43 @@
  * Multiplayer module exports
  */
 
-export * from './components'
-export * from './hooks'
-export * from './services'
+// Components
+export { RoomList } from './components/RoomList'
+export { RoomLobby } from './components/RoomLobby'
+export { ChatBox } from './components/ChatBox'
+export { OnlinePlayersList } from './components/OnlinePlayersList'
+export { ConnectionIndicator } from './components/ConnectionIndicator'
+export { CreateRoomModal } from './components/CreateRoomModal'
+export { MatchmakingQueue } from './components/MatchmakingQueue'
+
+// Hooks
+export { useWebSocket } from './hooks/useWebSocket'
+export { useWebSocketEvent } from './hooks/useWebSocketEvent'
+export { useRoom } from './hooks/useRoom'
+export { useChat } from './hooks/useChat'
+export { usePresence } from './hooks/usePresence'
+export { useSync } from './hooks/useSync'
+export { useLatency } from './hooks/useLatency'
+export { useMatchmaking } from './hooks/useMatchmaking'
+export { useMultiplayerStore } from './hooks/useMultiplayerStore'
+
+// Services
+export { WebSocketService } from './services/WebSocketService'
+export { RoomService } from './services/RoomService'
+export { ChatService } from './services/ChatService'
+export { PresenceService } from './services/PresenceService'
+export { SyncService } from './services/SyncService'
+export { LatencyService } from './services/LatencyService'
+export { MatchmakingService } from './services/MatchmakingService'
+
+// Types
+export type * from './types'
+
+// Utils
 export * from './utils'
+
+// Constants
+export * from './constants'
+
+// Providers
+export { MultiplayerProvider } from './providers/MultiplayerProvider'
